608285b0c116da4a9339185f4d17c413f71195b4
changmen1 <zxl17339811909@163.com>
2025-06-10 22:19:07 +0800

    feat:init tailwindcss

---
+++ electron/notes.md
@@ -0,0 +1,5 @@
+# HL7
+
+## go解析参考
+
+> <https://blog.csdn.net/qq_41467216/article/details/141647188>
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { handleTest } from './hooks/msg'
 function App() {
   return (
     <>
-      <h1>VitaLink</h1>
+      <h1 className='text-3xl font-bold bg-red-700'>VitaLink</h1>
+      <div className='text-blue-700'>哈哈哈哈</div>
       <button onClick={() => handleTest('红烧罗非鱼', '记得每早8点上班哦')}>消息通知</button>
     </>
   )
